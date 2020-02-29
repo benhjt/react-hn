dd72337edafeafcae3afa1e6bb54e430430fbff7
evaluethomasb <ben.thomas@ev.uk>
2020-02-29 14:35:11 +0000

    Adds NavLinks to navigate between pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,63 @@
 import React from 'react';
 
+import styled from 'styled-components';
+import { Link, NavLink } from 'react-router-dom';
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  &.active {
+    text-decoration: underline;
+  }
+`;
+
 function Header() {
+  const isActive = (path) => {
+    return function isActive(match, location) {
+      return location.pathname.indexOf(path) !== -1;
+    };
+  };
+
   return (
-    <header>
-      Header
-    </header>
+    <nav>
+      <Link to="/news/1">
+        Logo Here
+      </Link>
+      <StyledNavLink
+        activeClassName="active"
+        to="/news/1"
+        isActive={isActive('/news')}
+      >
+        Top
+      </StyledNavLink>
+      <StyledNavLink
+        activeClassName="active"
+        to="/newest/1"
+        isActive={isActive('/newest')}
+      >
+        New
+      </StyledNavLink>
+      <StyledNavLink
+        activeClassName="active"
+        to="/show/1"
+        isActive={isActive('/show')}
+      >
+        Show
+      </StyledNavLink>
+      <StyledNavLink
+        activeClassName="active"
+        to="/ask/1"
+        isActive={isActive('/ask')}
+      >
+        Ask
+      </StyledNavLink>
+      <StyledNavLink
+        activeClassName="active"
+        to="/jobs/1"
+        isActive={isActive('/jobs')}
+      >
+        Jobs
+      </StyledNavLink>
+    </nav>
   )
 }
 
