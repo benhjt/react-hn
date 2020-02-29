abf998c9819f8bd6f1df73444c0a498b49aca1ba
evaluethomasb <ben.thomas@ev.uk>
2020-02-29 14:34:40 +0000

    Adds router to route between pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Header from '../Header';
+import Story from '../Story';
+import User from '../User';
+import Feed from '../Feed';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
-        Main content
+        <Switch>
+          <Route path="/story/:id" component={Story} />
+          <Route path="/user/:id" component={User} />
+          <Route path="/news/:page" component={Feed} />
+          <Route path="/newest/:page" component={Feed} />
+          <Route path="/show/:page" component={Feed} />
+          <Route path="/ask/:page" component={Feed} />
+          <Route path="/jobs/:page" component={Feed} />
+          <Redirect to="/news/1" />
+        </Switch>
       </main>
-    </>
+    </Router>
   )
 }
 
