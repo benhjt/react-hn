49049a54681a89427fdcb14297db3960cade6e95
evaluethomasb <ben.thomas@ev.uk>
2020-03-01 11:29:51 +0000

    Adds the feed item and fixes eslint issues

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+import FeedItem from '../FeedItem';
+
 function Feed({ location }) {
   const [data, setData] = useState([]);
-  const feedType = location.pathname.split('/').slice(1)[0]
+  const feedType = location.pathname.split('/').slice(1)[0];
   useEffect(() => {
-    fetch(`https://node-hnapi.herokuapp.com/${feedType}?page=1`).then((response) => {
-      return response.json();
-    }).then((data) => {
-      setData(data);
+    fetch(`https://node-hnapi.herokuapp.com/${feedType}?page=1`).then((response) => response.json()).then((feedData) => {
+      setData(feedData);
     });
   }, [feedType]);
 
   return (
     <div>
       {data.map((item, index) => (
-        <div>
-          {item.id}
-        </div>
+        <FeedItem item={item} index={index} key={item.id} />
       ))}
     </div>
-  )
+  );
 }
 
 Feed.propTypes = {
-  location: PropTypes.shape({ pathname: PropTypes.string }).isRequired,
+  location: PropTypes.shape({ pathname: PropTypes.string }).isRequired
 };
 
 export default Feed;
