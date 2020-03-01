ec177312600ef2fd1fcd537782c8fd08c7d10a7d
evaluethomasb <ben.thomas@ev.uk>
2020-03-01 11:28:04 +0000

    Loads up the story data and prints the ID

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Story() {
+function Story({ location }) {
+  const [story, setStory] = useState({});
+  const storyId = location.pathname.split('/').slice(1)[1];
+  useEffect(() => {
+    fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`).then((response) => response.json()).then((storyData) => {
+      setStory(storyData);
+    });
+  }, [storyId]);
+
   return (
     <div>
-      Story
+      {story.id}
     </div>
-  )
+  );
 }
+
+Story.propTypes = {
+  location: PropTypes.shape({ pathname: PropTypes.string }).isRequired
+};
 
 export default Story;
