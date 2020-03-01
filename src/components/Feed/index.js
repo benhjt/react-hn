f71640b15a1d1bfb4fee3144fbba4e6fc89f18b8
evaluethomasb <ben.thomas@ev.uk>
2020-03-01 09:36:27 +0000

    Loads up the feed based on the URL

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function Feed() {
+function Feed({ location }) {
+  const [data, setData] = useState([]);
+  const feedType = location.pathname.split('/').slice(1)[0]
+  useEffect(() => {
+    fetch(`https://node-hnapi.herokuapp.com/${feedType}?page=1`).then((response) => {
+      return response.json();
+    }).then((data) => {
+      setData(data);
+    });
+  }, [feedType]);
+
   return (
     <div>
-      Feed
+      {data.map((item, index) => (
+        <div>
+          {item.id}
+        </div>
+      ))}
     </div>
   )
 }
+
+Feed.propTypes = {
+  location: PropTypes.shape({ pathname: PropTypes.string }).isRequired,
+};
 
 export default Feed;
