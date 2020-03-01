ca60b8ecdebf6e6f55240bf1b23f33aa5d7303aa
evaluethomasb <ben.thomas@ev.uk>
2020-03-01 11:28:19 +0000

    Loads up the user data and displays it

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function User() {
+function User({ location }) {
+  const [user, setUser] = useState({});
+  const username = location.pathname.split('/').slice(1)[1];
+  useEffect(() => {
+    fetch(`https://node-hnapi.herokuapp.com/user/${username}`).then((response) => response.json()).then((userData) => {
+      setUser(userData);
+    });
+  }, [username]);
+
   return (
-    <div>
-      User
-    </div>
-  )
+    <table>
+      <tbody>
+        <tr>
+          <td>user:</td>
+          <td>{user.id}</td>
+        </tr>
+        <tr>
+          <td>created:</td>
+          <td>{user.created_time}</td>
+        </tr>
+        <tr>
+          <td>karma:</td>
+          <td>{user.karma}</td>
+        </tr>
+        <tr>
+          <td>about:</td>
+          <td dangerouslySetInnerHTML={{ __html: user.about }} />
+        </tr>
+      </tbody>
+    </table>
+  );
 }
+
+User.propTypes = {
+  location: PropTypes.shape({ pathname: PropTypes.string }).isRequired
+};
 
 export default User;
